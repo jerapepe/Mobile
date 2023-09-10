0e940296907fa0b669918207171190881822d687
Jera <73955437+jerapepe@users.noreply.github.com>
2023-09-10 22:26:33 +0000

    Variables y tipos de datos

--- VariablesTiposDeDatos.kt
+++ VariablesTiposDeDatos.kt
@@ -1,9 +1,9 @@
 const val PI = 3.1416
 
 fun main( ) {
-    val num1 = 50.4541
+    val num1 = 34.3341
     println (num1)
-    val num2: Double = 5.5135
+    val num2: Double = 8.6535
     val num3: Float = num2.toFloat()
     println("Numero Double: $num2")
     println("Numero Float: $num3")
