ca7ee4c1769e6d612a90dc015b2a1f480e634d1d
Jera <73955437+jerapepe@users.noreply.github.com>
2023-09-10 22:23:35 +0000

    Variables y tipos de datos

---
+++ VariablesTiposDeDatos.kt
@@ -0,0 +1,15 @@
+const val PI = 3.1416
+
+fun main( ) {
+    val num1 = 50.4541
+    println (num1)
+    val num2: Double = 5.5135
+    val num3: Float = num2.toFloat()
+    println("Numero Double: $num2")
+    println("Numero Float: $num3")
+
+    val decimal= 10
+    val  resultado= 2 * PI * decimal
+
+    println("El perímetro del círculo es: $resultado")
+}
