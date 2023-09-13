c6a09da669b75ca9a271a1225b3483832d923a02
Jera <73955437+jerapepe@users.noreply.github.com>
2023-09-13 22:56:15 +0000

    cambios 4

--- kss.kt
+++ kss.kt
@@ -6,20 +6,21 @@ fun main( ) {
    
    //val coord1: Array<int> = arrayOf(4, 3)
    //val coord2: Array<int> = arrayOf(-3, -2)
-   val x1 = 4
+   	val x1 = 4
     val y1 = 3
     val x2 = -3
     val y2 = -2
-   val m = (y2 - y1) / (x2 - x1)
+   	val m = (y2 - y1) / (x2 - x1)
    
-   println(m)
+   	println(m)
    
-   val radius = 4f
-   val area = circleArea(radius)
-   println("El area del circulo es $area")
+  	val radius = 4f
+  	val area = circleArea(radius)
+  	println("El area del circulo es $area")
    
-   val pi = getPI()
-	println(pi)   
+   	val pi = getPI()
+	println(pi)
+    printPHI()
 }
 
 fun circleArea(radius:Float): Float{
@@ -29,4 +30,6 @@ fun circleArea(radius:Float): Float{
 fun getPI(): Float{
     return PI
 }
-
+fun printPHI(){
+    println("El numero aureo vale $PHI")
+}
