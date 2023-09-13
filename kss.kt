b4ea35172098eba9ba2c697a8113e288a50555c5
Jera <73955437+jerapepe@users.noreply.github.com>
2023-09-13 22:46:28 +0000

    cambios 2

--- kss.kt
+++ kss.kt
@@ -1,3 +1,25 @@
-fun main() {
-    println("Hola")
+import kotlin.math.pow
+const val PI = 3.1416f
+const val PHI = 1.618f
+
+fun main( ) {
+   
+   //val coord1: Array<int> = arrayOf(4, 3)
+   //val coord2: Array<int> = arrayOf(-3, -2)
+    
+   //val m = (y2 - y1) / (x2 - x1)
+   
+   //println(coord1[0])
+   
+   val radius = 4f
+    val area = circleArea(radius)
+    println("El area del circulo es $area")
+    
+   
+}
+fun circleArea(radius:Float): Float{
+    return PI*radius.pow(2)
+}
+fun circleArea(radius:Float): Float{
+    return PI*radius.pow(2)
 }
