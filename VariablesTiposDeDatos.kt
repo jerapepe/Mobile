b0f1901fb6886c2c14c5c8d75a70e9f10af1b71e
Jera <73955437+jerapepe@users.noreply.github.com>
2023-09-13 22:30:33 +0000

    cambios

--- VariablesTiposDeDatos.kt
+++ VariablesTiposDeDatos.kt
@@ -2,13 +2,14 @@ const val PI = 3.1416
 
 fun main( ) {
     val num1 = 34.3341
-    println (num1)
+    val tipo = num1::class
+    println (tipo)
     val num2: Double = 8.6535
     val num3: Float = num2.toFloat()
     println("Numero Double: $num2")
     println("Numero Float: $num3")
 
-    val decimal= 10
+    val decimal= 3.2584f
     val  resultado= 2 * PI * decimal
 
     println("El perímetro del círculo es: $resultado")
